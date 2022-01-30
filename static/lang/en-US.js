71d36ee69e922a3da91d12254d5af2afca942186
Ardhika Ryzha Nurmawan <ardhikarn@gmail.com>
2022-01-30 14:51:52 +0700

    configure i18n

---
+++ static/lang/en-US.js
@@ -0,0 +1,44 @@
+export default () => {
+  return new Promise(function(resolve) {
+    resolve({
+      header: {
+        "location": "Type your location",
+        "loginsignup": "Login/Sign Up",
+      },
+      header_title: {
+        "resto": "Restaurant",
+        "type": "Preferred Merchant",
+        "min": "mins",
+        "opening": "Opening Hours",
+        "details": "See details",
+        "promotions": "Promotions"
+      },
+      main: {
+        "info": "We're always working to get the most accurate information. Let us know if you come across anything that's outdated!"
+      },
+      footer: {
+        "en": "EN",
+        "id": "ID",
+        "frequently": "Frequently Searched",
+        "popular": "Popular Cuisines",
+        "chinese": "Frequently Searched",
+        "fast": "Frequently Searched",
+        "indian": "Frequently Searched",
+        "indonesian": "Frequently Searched",
+        "italian": "Frequently Searched",
+        "japanese": "Frequently Searched",
+        "korean": "Frequently Searched",
+        "thai": "Frequently Searched",
+        "vietnamese": "Frequently Searched",
+        "about_grab": "Frequently Searched",
+        "about_grabfood": "Frequently Searched",
+        "blog": "Frequently Searched",
+        "support": "Support",
+        "help": "Help",
+        "faqs": "FAQs",
+        "merchant": "Be a GrabFood Merchant",
+        "drive": "Drive With Grab",
+      }
+    })
+  })
+}
---
+++ static/lang/id-ID.js
@@ -0,0 +1,44 @@
+export default () => {
+  return new Promise(function(resolve) {
+    resolve({
+      header: {
+        "location": "Masukin lokasimu",
+        "loginsignup": "Masuk/Daftar",
+      },
+      header_title: {
+        "resto": "Restoran",
+        "type": "Restoran Pilihan",
+        "min": "mnt",
+        "opening": "Jam Buka",
+        "details": "Lihat detail",
+        "promotions": "Promosi"
+      },
+      main: {
+        "info": "Kami selalu berusaha supaya informasi kami tepat dan akurat. Kasih tahu ya kalau ada yang salah."
+      },
+      footer: {
+        "en": "EN",
+        "id": "ID",
+        "frequently": "Frequently Searched",
+        "popular": "Popular Cuisines",
+        "chinese": "Frequently Searched",
+        "fast": "Frequently Searched",
+        "indian": "Frequently Searched",
+        "indonesian": "Frequently Searched",
+        "italian": "Frequently Searched",
+        "japanese": "Frequently Searched",
+        "korean": "Frequently Searched",
+        "thai": "Frequently Searched",
+        "vietnamese": "Frequently Searched",
+        "about_grab": "Frequently Searched",
+        "about_grabfood": "Tentang GrabFood",
+        "blog": "Frequently Searched",
+        "support": "Bantuan",
+        "help": "Bantuan",
+        "faqs": "Yang Sering Ditanya",
+        "merchant": "Yuk Jadi Mitra Restoran GrabFood",
+        "drive": "Yuk Jadi Mitra Pengemudi Grab",
+      }
+    })
+  })
+}
