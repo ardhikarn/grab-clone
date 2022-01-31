211049f5d098df4591952a441d935a937e05dbf8
Ardhika Ryzha Nurmawan <ardhikarn@gmail.com>
2022-01-31 14:07:53 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,17 +19,23 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
+    script: [
+      // { src: "https://code.jquery.com/jquery-migrate-1.2.1.min.js" },
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
-      { rel: 'stylesheet', href: 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css' }
+      { rel: 'stylesheet', href: 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css' },
+      { rel: 'stylesheet', href: 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css' },
     ],
     
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/vuetify-overide.scss'
+    '~/assets/vuetify-overide.scss',
+    '~/assets/vendors/slick-carousel/slick.css',
+    '~/assets/vendors/slick-carousel/slick-theme.css'
   ],
 
   loading: { color: theme.primary },
@@ -38,6 +44,8 @@ export default {
   plugins: [
     { src: '~plugins/i18n-config.js' },
     { src: '~/plugins/vue-scroll-nav', ssr: false },
+    { src: '~/plugins/caroussel-config', ssr: false },
+    { src: '~/plugins/flickity', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
